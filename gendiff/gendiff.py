e84f6c353fec95771c2cbce102f43494a2c4288d
Viktor Sklyuev <sklyevv@yandex.ru>
2023-03-03 14:54:01 +0300

    write some code

---
+++ gendiff/gendiff.py
@@ -0,0 +1,10 @@
+import json
+
+
+def gendiff(file_one, file_two):
+    json_one = json.load(open(file_one))
+    json_two = json.load(open(file_two))
+
+    difference_string = ''
+
+    return difference_string
