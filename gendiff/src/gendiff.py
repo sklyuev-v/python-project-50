d6e57004598b8df3fe6971ab1722d191984c7e37
Viktor Sklyuev <sklyevv@yandex.ru>
2024-09-10 14:28:20 +0300

    Add docstring and types annotations

--- gendiff/src/gendiff.py
+++ gendiff/src/gendiff.py
@@ -1,14 +1,26 @@
-from gendiff.src.file_work import read_from_file
-from gendiff.src.data_loader import load_json
+from typing import Callable
+
+from gendiff.src.file_handlers import collect_file_data
+from gendiff.src.data_loader import data_loader
 from gendiff.src.diff_generator import get_diff
-from gendiff.src.formatter.stylish import stylish_render
+from gendiff.src.formatter.visualizer import visualize_tree
+from gendiff.src.formatter.visualizer import DEFAULT_FORMAT
 
 
-def generate_diff(filepath_1: str, filepath_2: str) -> str:
-    content_1 = load_json(read_from_file(filepath_1))
-    content_2 = load_json(read_from_file(filepath_2))
+def generate_diff(filepath_1: str, filepath_2: str,
+                  format: str = DEFAULT_FORMAT) -> Callable[[dict], str]:
+    """Accept the entered data and returns the difference in the selected format
 
-    result = get_diff(content_1, content_2)
-    return stylish_render(result)
+    Args:
+        filepath_1 (str): path to first file
+        filepath_2 (str): path to second file
+        format (str, optional): format to comparison. Default: stylish.
 
-    
+    Returns:
+        Callable[[dict], str]:visualize difference tree
+    """
+    content_1 = data_loader(*collect_file_data(filepath_1))
+    content_2 = data_loader(*collect_file_data(filepath_2))
+
+    diff_tree = get_diff(content_1, content_2)
+    return visualize_tree(diff_tree, format)
