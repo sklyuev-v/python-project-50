eef6779ef0f095b57e08897f3ae300a4f943cfe4
Viktor Sklyuev <sklyevv@yandex.ru>
2024-09-10 14:15:13 +0300

    Add recursive traversal of nested dicts

--- gendiff/src/diff_generator.py
+++ gendiff/src/diff_generator.py
@@ -1,22 +1,48 @@
+from typing import Any, Optional
+
+
+REMOVED = 'removed'
+ADDED = 'added'
+UNCHANGED = 'unchanged'
+NESTED = 'nested'
+UPDATED = 'updated'
+
+
 def get_diff(data1: dict, data2: dict) -> dict:
-    
+    """Accept two dictionary and return their difference tree
+
+    Args:
+        data1 (dict): data from first file as a python dictionary
+        data2 (dict): data from second file as a python dictionary
+
+    Returns:
+        dict: difference tree of the first file (data1) and second file (data2)
+    """
     all_keys = sorted(set(data1.keys()) | set(data2.keys()))
 
     diff = []
 
     for key in all_keys:
         if key in data1 and key not in data2:
-            diff.append(add_node(key, 'removed', oldValue=data1[key]))
+            diff.append(add_node(key, REMOVED, oldValue=data1[key]))
         elif key not in data1 and key in data2:
-            diff.append(add_node(key, 'added', value=data2[key]))
+            diff.append(add_node(key, REMOVED, value=data2[key]))
         elif data1[key] == data2[key]:
-            diff.append(add_node(key, 'unchanged', oldValue=data1[key]))
+            diff.append(add_node(key, UNCHANGED, oldValue=data1[key]))
+        elif isinstance(data1[key], dict) and isinstance(data2[key], dict):
+            child = get_diff(data1[key], data2[key])
+            diff.append(add_node(key, NESTED, children=child))
         else:
-            diff.append(add_node(key, 'updated', value=data2[key], oldValue=data1[key]))
+            diff.append(
+                add_node(key, UPDATED, value=data2[key], oldValue=data1[key])
+            )
     return diff
 
 
-def add_node(key, node_type, value=None, oldValue=None):
+def add_node(key: Any, node_type: str,
+             value: Any = None, oldValue: Any = None,
+             children: Optional[list] = None) -> dict:
+
     node = {
         'key': key,
         'value': {
@@ -26,5 +52,7 @@ def add_node(key, node_type, value=None, oldValue=None):
         'node_type': node_type
     }
 
-    return node
+    if children is not None:
+        node['children'] = children
 
+    return node
