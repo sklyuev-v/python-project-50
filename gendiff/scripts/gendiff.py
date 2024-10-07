7cda3017dd2fedebfb29259f844c9985a8106c5b
Sklyuev Viktor <sklyevv@yandex.ru>
2024-10-07 20:50:35 +0300

    add type annotation and fix mistake

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,14 +1,16 @@
 # !usr/bin/env python3
+from typing import NoReturn
+
 from gendiff.src.cli import parse_arguments
 from gendiff.src.gendiff import generate_diff
 
 
-def main():
+def main() -> NoReturn:
     """
     Entry point for application
 
     Return:
-        Print results of the generate_diff function()
+        Print result of the generate_diff function()
     """
     args = parse_arguments()
     try:
